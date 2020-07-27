9450513c773ebd2fa1ca18c93c58d21e8e2945f4
HP <efafafaef>
2020-07-27 15:21:22 +0300

    feat: add task 2 script

--- script.js
+++ script.js
@@ -23,20 +23,82 @@ function changeArray(number, index) {
     } 
 }
 
-let number = +prompt('Сколько чисел Вы хотите добавить в массив?');
+let number = prompt('Сколько чисел Вы хотите добавить в массив?');
+
 while(true) {
-    if (typeof number !== 'number') {
-        number = +prompt('Необходимо ввести число');
-    } else break;
+    if (number.match(/\D/gu)) {
+        number = prompt('Необходимо ввести число');
+    } else {
+        number = +number;
+        break;
+    };
 }
 
-let index = +prompt('Укажите коэффициент изменения массива');
+let index = prompt('Укажите коэффициент изменения массива');
+
 while(true) {
-    if (typeof index !== 'number' || index === 0) {
-        index = +prompt('Необходимо ввести положительное число');
-    } else break;
+    if (index.match(/\D/gu) || index == 0) {
+        index = prompt('Необходимо ввести положительное число');
+    } else {
+        index = +index;
+        break;
+    };
 }
 
 console.log(array);
 array = changeArray(number, index);
 console.log(array);
+
+// Task 2
+
+function getPartOfString(string, start, end) {
+    if(start === 0 && end === 0) {
+    return string;
+
+    } else if(start === end) {
+        return string.slice(start, end + 1);
+
+    } else if(start < 0 && end >= 0) {
+        return string.slice(end, start);
+
+    } else if(start > 0 && end < 0) {
+        return string.slice(start, end);
+
+    } else if(start < 0 && end < 0) {
+        if(start < end) {
+            return string.slice(start, end);
+        } else return string.slice(end, start);
+
+    } else if(start > end) {
+        return string.substring(start, end);
+
+    } else if (start >= string.length || end >= string.length) {
+        return string.substring(start, end);
+
+    } else return string.slice(start, end);
+}
+
+let userString = prompt('Напишите что-нибудь длинное');
+
+let startIndex = prompt('Укажите начало диапазона для извлечения. Нумерация начинается с 0.');
+
+while(true) {
+    if (startIndex.match(/\D/gu)) {
+        startIndex = prompt('Необходимо ввести число');
+    } else {
+        startIndex = +startIndex;
+        break;
+    };
+}
+
+let endIndex = prompt('Укажите конец диапазона для извлечения. Нумерация начинается с 0.');
+while(true) {
+    if (endIndex.match(/\D/gu)) {
+        endIndex = prompt('Необходимо ввести число');
+    } else {
+        endIndex = +endIndex;
+        break;
+    };
+}
+
+console.log(getPartOfString(userString, startIndex, endIndex));
