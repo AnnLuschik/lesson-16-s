ddfe0674163b519bbd7adfe1b0c82f517f6a1aa6
HP <efafafaef>
2020-07-28 17:16:30 +0300

    feat: add task 5 script

---
+++ task5.js
@@ -0,0 +1,24 @@
+let family = {
+    amount: 5,
+    familyMembers: [
+        {name: 'Borya', age: 15},
+        {name: 'Tolya', age: 54},
+        {name: 'Anjela', age: 45},
+        {name: 'Snejana', age: 14},
+        {name: 'Gavriil', age: 24},
+    ],
+    addOldestPerson() {
+        let oldestPerson;
+        let oldestPersonAge = 0;
+        for (let person of this.familyMembers) {
+            if (oldestPersonAge < person.age) {
+                oldestPersonAge = person.age;
+                oldestPerson = person;
+            } 
+        }
+        this.oldestFamilyMember = oldestPerson;
+    }
+}
+
+family.addOldestPerson();
+console.log(family);
