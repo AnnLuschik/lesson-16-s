e751146d2fe5acd2bd42f36cecbe1c5163c160db
HP <efafafaef>
2020-07-28 10:59:31 +0300

    delete strict validation in task 1

--- task1.js
+++ task1.js
@@ -7,42 +7,38 @@ function changeArray(number, index) {
 
     let operation = prompt('Увеличить или уменьшить массив?');
 
-    while(true) {
-        if (operation.toLowerCase() === 'увеличить') {
-            return array.map(function(item) {
-                return item = item * index;
-            });
-    
-        } else if (operation.toLowerCase() === 'уменьшить') {
-            return array.map(function(item) {
-                return item = item / index;
-            });
+    if (operation.toLowerCase() === 'увеличить') {
+        return array.map(function(item) {
+            return item = item * index;
+        });
 
-        } else operation = prompt('Недопустимое значение. Выберите из двух вариантов: увеличить либо уменьшить.');
-    } 
+    } else if (operation.toLowerCase() === 'уменьшить') {
+        return array.map(function(item) {
+            return item = item / index;
+        });
+
+    } else operation = prompt('Недопустимое значение. Выберите из двух вариантов: увеличить либо уменьшить.');   
 }
 
 let number = prompt('Сколько чисел Вы хотите добавить в массив?');
 
-while(true) {
-    if (number.match(/\D/gu)) {
-        number = prompt('Необходимо ввести число');
-    } else {
-        number = +number;
-        break;
-    };
-}
+// Проверка на ввод числа
+if (number.match(/\D/gu)) {
+    number = prompt('Необходимо ввести число');
+} else {
+    number = +number;
+    break;
+};    
 
 let index = prompt('Укажите коэффициент изменения массива');
 
-while(true) {
-    if (index.match(/\D/gu) || index == 0) {
-        index = prompt('Необходимо ввести положительное число');
-    } else {
-        index = +index;
-        break;
-    };
-}
+// Проверка, что введено число больше 0
+if (index.match(/\D/gu) || index == 0) {
+    index = prompt('Необходимо ввести положительное число');
+} else {
+    index = +index;
+    break;
+};
 
 console.log(array);
 array = changeArray(number, index);
